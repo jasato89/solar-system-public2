60689a1ce1cd0129498bddee0954e8f83a2c01dc
Jaume <jasato89@gmail.com>
2020-11-18 21:23:21 +0100

    Iteración 3

--- lib/planetProvider.dart
+++ lib/planetProvider.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class PlanetProvider {
 
-  List<Widget> planets = [];
   final Map<String, String> planetMap = {
     'Earth': 'earth.png',
     'Venus': 'venus.png',
@@ -14,6 +13,8 @@ class PlanetProvider {
     'Sun' : 'sun.png',
     'Uranus': 'uranus.png',
   };
+
+  List<int> distance = [149, 108, 57, 780, 228, 4530, 5934, 0, 2871];
 
 
 
@@ -30,7 +31,10 @@ class PlanetProvider {
         return ListTile(
           leading: Image.asset('resources/images/${planetMap.values.toList()[index]}'),
           title: Text('${planetMap.keys.toList()[index]}'),
-          trailing: Text('>'),
+          subtitle: Text('${distance[index]} millions of kms to the Sun'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+          dense: true,
         );
       });
 
